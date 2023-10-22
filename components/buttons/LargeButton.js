9f325e9916e52dd2bf8b0bfb1b98558d3a1ee4c6
Marilyne Rios <rios.marilyne@gmail.com>
2023-10-22 16:31:42 +0200

    bug largeButton n'était pas mergé test 2

---
+++ components/buttons/LargeButton.js
@@ -0,0 +1,61 @@
+/*
+@param {string} title
+@param {function} onPress
+*/
+
+import { TouchableOpacity, Text, View, StyleSheet} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useFonts } from 'expo-font';
+
+const SmallButton  = ({ title, onPress  }) => {
+    
+  return (
+    <View style={styles.smallButtonContainer}>
+   
+        <LinearGradient
+          colors={["transparent", "#004644"]}
+          style={styles.buttonBackground}
+        >
+          <TouchableOpacity
+            style={styles.button}
+            onPress={onPress}
+          >
+            <Text style={styles.buttonText}>{title}</Text>
+          </TouchableOpacity>
+        </LinearGradient>
+    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+  smallButtonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button:{
+    padding: 10,
+    width: 320,
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+   
+  },
+  buttonBackground: {
+    height: '50%',
+    width: '100%',
+   
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+  buttonText: {
+    fontSize: 18,
+    color: "#004644",
+    fontFamily: "Indie-Flower",
+  },
+});
+
+export default SmallButton;
