dca7c04a855416ed2ac9ea6ff49d01c3439e54ee
AnthonyM <134475916+Anthony101083@users.noreply.github.com>
2023-10-24 14:33:53 +0200

    anthony homescreen buttons

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,93 +1,95 @@
-import {TouchableOpacity, Text, View, StyleSheet, Image} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useFonts } from 'expo-font';
-import HeaderReturn from '../components/HeaderReturn';
-import SmallButton from '../components/buttons/SmallButton';
-import CalendarContainer from '../components/CalendarContainer';
+import { TouchableOpacity, Text, View, StyleSheet, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useFonts } from "expo-font";
+import HeaderReturn from "../components/HeaderReturn";
+import SmallButton from "../components/buttons/SmallButton";
+import CalendarContainer from "../components/CalendarContainer";
 
-export default function CalendarScreen ({navigation}) {
-    const [fontsLoaded] = useFonts({
-        'Indie-Flower': require('../assets/fonts/IndieFlower-Regular.ttf'),
-    });
+export default function CalendarScreen({ navigation }) {
+  const [fontsLoaded] = useFonts({
+    "Indie-Flower": require("../assets/fonts/IndieFlower-Regular.ttf"),
+  });
 
-    if (!fontsLoaded) {
+  if (!fontsLoaded) {
     return null;
-    }
+  }
 
-    const handleValidate = () => {
-        //dispatch activité
-        navigation.navigate('Who');
-    }
-    
-    return (
-        <View style={styles.container}>
-            <LinearGradient colors={['#D9F2B1', 'transparent']}  style={styles.background} >
-                <HeaderReturn iconContext="profil" pages="profil" />
+  const handleValidate = () => {
+    //dispatch activité
+    navigation.navigate("Who");
+  };
 
-                <View style={styles.bodyContainer}>
-                    <View style={styles.titleContainer}>
-                        <Text style={styles.title}>Quand voulez-vous pratiquer votre activité ?</Text>
-                    </View>
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={["#D9F2B1", "transparent"]}
+        style={styles.background}
+      >
+        <HeaderReturn iconContext="profil" pages="Profil" />
 
-                    <View style={styles.infoContainer}>
-                        <CalendarContainer />
-                    </View>
+        <View style={styles.bodyContainer}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>
+              Quand voulez-vous pratiquer votre activité ?
+            </Text>
+          </View>
 
-                    <View style={styles.validateContainer}>
-                        <SmallButton title='Valider' onPress={handleValidate} />
-                    </View>
-                </View>
-                
+          <View style={styles.infoContainer}>
+            <CalendarContainer />
+          </View>
 
-                
-            </LinearGradient>
+          <View style={styles.validateContainer}>
+            <SmallButton title="Valider" onPress={handleValidate} />
+          </View>
         </View>
-    )
+      </LinearGradient>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'transparent',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    background: {
-        height: '100%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-    bodyContainer: {
-        height: '80%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-    titleContainer: {
-        height: '20%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 10,
-    },
-    title: {
-        fontSize: 22,
-        fontFamily: 'Indie-Flower',
-        color: '#004644',
-        textAlign: 'center',
-    },
-    infoContainer:{
-        height: '60%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderWidth: 1,
-    },
-    validateContainer: {
-        height: '20%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  background: {
+    height: "100%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  bodyContainer: {
+    height: "80%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  titleContainer: {
+    height: "20%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  title: {
+    fontSize: 22,
+    fontFamily: "Indie-Flower",
+    color: "#004644",
+    textAlign: "center",
+  },
+  infoContainer: {
+    height: "60%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+  },
+  validateContainer: {
+    height: "20%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,108 +1,117 @@
-import {TouchableOpacity, Text, View, StyleSheet, Image} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useFonts } from 'expo-font';
-import Logo from '../assets/Free-Time-logos_black.svg';
+import { TouchableOpacity, Text, View, StyleSheet, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useFonts } from "expo-font";
+import Logo from "../assets/Free-Time-logos_black.svg";
+import SmallButton from "../components/buttons/SmallButton";
 
-export default function HomeScreen ({navigation}) {
-    const [fontsLoaded] = useFonts({
-        'Indie-Flower': require('../assets/fonts/IndieFlower-Regular.ttf'),
-    });
+export default function HomeScreen({ navigation }) {
+  const [fontsLoaded] = useFonts({
+    "Indie-Flower": require("../assets/fonts/IndieFlower-Regular.ttf"),
+  });
 
-    if (!fontsLoaded) {
+  if (!fontsLoaded) {
     return null;
-    }
+  }
 
-    return (
-        <View style={styles.container}>
-            <LinearGradient colors={['#D9F2B1', 'transparent']}  style={styles.background} >
-                <View style={styles.welcomeContainer}>
-                    <Text style={styles.welcome}>Welcome to</Text>
-                </View>
-                <View style={styles.logoContainer}>
-                    <Logo width='125%'/>
-                </View>
-
-                <View style={styles.buttonContainer}>
-                    <View style={styles.gradientContainer}>
-                        <LinearGradient colors={['transparent', '#004644']}  style={styles.background}>
-                            <TouchableOpacity style={styles.buttonBackground} onPress={() => navigation.navigate('CreateProfil')}>
-                                <Text>First</Text>
-                            </TouchableOpacity>
-                        </LinearGradient>
-                    </View>
-
-                    <View style={styles.gradientContainer}>
-                        <LinearGradient  colors={['transparent', '#004644']}  style={styles.background}>
-                            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Connection')}>
-                                <Text>Connexion</Text>
-                            </TouchableOpacity>
-                        </LinearGradient>
-                    </View>
-                </View>
-            </LinearGradient>
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={["#D9F2B1", "transparent"]}
+        style={styles.background}
+      >
+        <View style={styles.welcomeContainer}>
+          <Text style={styles.welcome}>Welcome to</Text>
         </View>
-    )
+        <View style={styles.logoContainer}>
+          <Logo width="125%" />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <View style={styles.buttonBackgroundUp}>
+            <SmallButton
+              title="S'inscrire"
+              onPress={() => navigation.navigate("CreateProfil")}
+              style={styles.buttonSignUp}
+            >
+              <Text style={{ fontSize: 10 }}>S'inscrire</Text>
+            </SmallButton>
+          </View>
+
+          <View style={styles.buttonBackgroundIn}>
+            <SmallButton
+              title="Se connecter"
+              onPress={() => navigation.navigate("Connection")}
+              style={styles.buttonSignIn}
+            >
+              <Text style={{ fontSize: 10 }}>S'inscrire</Text>
+            </SmallButton>
+          </View>
+        </View>
+      </LinearGradient>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'transparent',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    gradientContainer: {
-        height: '20%',
-        width: '60%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    background: {
-        height: '100%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        borderRadius: 50,
-    },
-    welcomeContainer: {
-        height: '25%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 40,
-    },
-    welcome: {
-        color: '#004644',
-        fontSize: 50,
-        fontFamily: 'Indie-Flower'
-    },
-    logoContainer: {
-        height: '25%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    logo: {
-        height: '100%',
-        width: '125%',
-    },
-    buttonBackground: {
-        height: '50%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    button: {
-        height: '100%',
-        width: '100%',
-        margin: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    buttonContainer: {
-        height: '45%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  //   gradientContainer: {
+  //     height: "20%",
+  //     width: "60%",
+  //     alignItems: "center",
+  //     justifyContent: "center",
+  //   },
+  background: {
+    height: "100%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    borderRadius: 50,
+  },
+  welcomeContainer: {
+    height: "25%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+  },
+  welcome: {
+    color: "#004644",
+    fontSize: 50,
+    fontFamily: "Indie-Flower",
+  },
+  logoContainer: {
+    height: "25%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    height: "100%",
+    width: "125%",
+  },
+  buttonContainer: {
+    height: "60%",
+    width: "100%",
+    alignItems: "center",
+    // justifyContent: "space-around",
+  },
+
+  buttonBackgroundUp: {
+    height: 110,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonBackgroundIn: {
+    height: 110,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 16,
+  },
+});
