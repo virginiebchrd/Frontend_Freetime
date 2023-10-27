fb0c8184111b7d4bffca079ebce5430a02b1d454
AnthonyM <134475916+Anthony101083@users.noreply.github.com>
2023-10-27 12:03:18 +0200

    anthony homescreen css button large radius police 19

--- components/buttons/LoginButtons.js
+++ components/buttons/LoginButtons.js
@@ -1,59 +1,46 @@
-
-
-import { TouchableOpacity, Text, View, StyleSheet} from "react-native";
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
 
-
-
-const LoginButtons  = ({ title, onPress  }) => {
-    
-   
-
+const LoginButtons = ({ title, onPress }) => {
   return (
     <View style={styles.loginButtonsContainer}>
-   
-        <LinearGradient
-          colors={["transparent", "#004644"]}
-          style={styles.buttonBackground}
-        >
-          <TouchableOpacity
-            style={styles.button}
-            onPress={onPress}
-          >
-            <Text style={styles.buttonText}>{title}</Text>
-          </TouchableOpacity>
-        </LinearGradient>
-    
+      <LinearGradient
+        colors={["transparent", "#004644"]}
+        style={styles.buttonBackground}
+      >
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <Text style={styles.buttonText}>{title}</Text>
+        </TouchableOpacity>
+      </LinearGradient>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
- 
-    loginButtonsContainer: {
+  loginButtonsContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  button:{
+  button: {
     padding: 10,
-    width: 162,
-    height: 100,
-    alignItems: 'center',
-    justifyContent: 'center',
-   
+    width: 202,
+    height: '100%', 
+    alignItems: "center",
+    justifyContent: "center",
+    // backgroundColor: "blue",
   },
   buttonBackground: {
-    height: '50%',
-    width: '100%',
-   
-    borderRadius: 25,
-    alignItems: 'center',
-    justifyContent: 'center',
-},
+    height: "50%",
+    width: "100%",
+
+    borderRadius: 45,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   buttonText: {
-    fontSize: 16,
+    fontSize: 22,
     color: "#004644",
     fontWeight: "600",
     fontFamily: "Indie-Flower",
