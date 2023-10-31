32f141dd2119c0f31a262e8f04d4adc65f41872d
AnthonyM <134475916+Anthony101083@users.noreply.github.com>
2023-10-31 16:03:29 +0100

    anthony KeyboardAvoidingView sur connectionscreen

--- screens/ConnectionScreen.js
+++ screens/ConnectionScreen.js
@@ -155,7 +155,8 @@ export default function FirstConnectionScreen({ navigation }) {
               onPress={handleConnection}
             />
           </View>
-        )}
+          )}
+          </View>
       </KeyboardAvoidingView>
     </LinearGradient>
   );
